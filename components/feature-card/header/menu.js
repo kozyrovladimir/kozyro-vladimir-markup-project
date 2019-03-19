4495ca6658c132eebbb57a70949c1ed30b9cd4b1
kozyro.vladimir <kozyrovladimir@gmail.com>
2019-03-19 23:33:16 +0300

    add adaptive features

--- components/feature-card/header/menu.js
+++ components/feature-card/header/menu.js
@@ -5,25 +5,17 @@ $(document).ready(function() {
 });
 $(window).resize(function() {
     if ($(window).width() > 960) {
-        $('.navigation ul').removeAttr('style');
+        $('.navigation ul').removeAttr();
     }
 })
 
 $(document).ready(function(){
     $("#header").removeClass("fix-header-big");
-    $("#header").removeClass("fix-header-small");
     $(window).scroll(function(){
         if  (($(this).scrollTop() > 20) && ($(window).width() > 960)) {
             $("#header").addClass("fix-header-big").fadeIn('fast');
         } else {
             $("#header").removeClass("fix-header-big").fadeIn('fast');
-        };
-    });
-    $(window).scroll(function(){
-        if  (($(this).scrollTop() > 20) && ($(window).width() < 960)) {
-            $("#header").addClass("fix-header-small").fadeIn('fast');
-        } else {
-            $("#header").removeClass("fix-header-small").fadeIn('fast');
         };
     });
 });
